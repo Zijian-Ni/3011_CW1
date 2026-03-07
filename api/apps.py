19cab83a0d63af5bdfc8d55f48b546a6b88fac51
Zijian Ni <146772890+Zijian-Ni@users.noreply.github.com>
2026-03-07 22:32:58 +0000

    Add ApiConfig class to configure the API app

--- api/apps.py
+++ api/apps.py
@@ -1,1 +1,6 @@
+from django.app import AppConfig
 
+class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api'
+    path = str(__file__).resolve().parent)
